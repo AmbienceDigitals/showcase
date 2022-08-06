e8ac88f400d3bd3d758e98d6305e22826cdb1d05
Ezekiel Ayano <ezekielayano@gmail.com>
2022-08-06 17:56:16 +0100

    landing page for NFT mobile application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {Download, Features, SectionWrapper} from './components'
+import assets from './assets';
+import styles from './styles/Global';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+      title="Your own store of NFTs,
+      Start Selling & Growing"
+      description="Buy, sell, collect NFTs, exchange and earn crypto"
+      showBtn
+      mockupImg={assets.homeHero}
+      banner="banner"/>
+
+      <SectionWrapper
+      title="Smart User Interface Marketplace"
+      description="Experience the buttery UI of ProNef NFT Marketplace
+      with smooth UI design"
+      mockupImg={assets.homeCards}
+      reverse/>
+
+      <Features/>
+
+      <SectionWrapper
+      title="Deployment"
+      description="Built using Expo which runs natively on both Android and IOS devices. You can easily get your mobile applications up and running with Expo"
+      mockupImg={assets.feature}
+      reverse/>
+
+      <SectionWrapper
+      title="Showcase your Nft store in a creative manner"
+      description="The application affords users the opportunity to list their NFT and also includes a screen to give details and ratings for a particular NFT "
+      mockupImg={assets.mockup}
+      banner="banner02"/>
+
+      <Download/>
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+        <span className = "bold">Ambience Digital Solutions</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from '../styles/Global';
+import assets from '../assets';
+import Button from './Button'
+
+
+const SectionWrapper = ({title, description, showBtn, mockupImg, banner, reverse}) => {
+    return (
+        <div className={`min-h-screen 
+        ${styles.section} 
+        ${reverse ?styles.bgWhite : styles.bgPrimary} 
+        ${banner}`}>
+            {/* set animations and text alignment conditionally  */}
+            <div className={`flex items-center 
+            ${reverse? styles.boxReverseClass :styles.boxClass} 
+            w-11/12 sm:full minmd:w-3/4`}>
+                <div className={`${styles.descDiv}
+                ${reverse ? " fadeRightMini": "fadeLeftMini"}
+                ${reverse ? styles.textRight: styles.textLeft }`}>
+                    {/* conditionally set text color */}
+                    <h1 className={`
+                    ${reverse ? styles.blackText: styles.whiteText }
+                    ${styles.h1Text}`}>{title}</h1>
+                    <h1 className={`${reverse ? styles.blackText: styles.whiteText }
+                    ${styles.description}`}>{description}</h1>
+                    {/* display the button conditionally */}
+                    {showBtn && (
+                        <Button
+                        assetUrl={assets.expo}
+                        link="https://expo.dev/@ambience_media/nf
+                        t?serviceType=classic&distribution=expo-go" />
+                    )}
+                </div>
+
+                <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0 `}>
+                    {/* conditionally animate image in opposite direction of text animation */}
+                    <img className={`${reverse ? " fadeLeftRMini": "fadeRightMini"}
+                        styles.sectionImg}`} src={mockupImg} alt="Section Wrapper" />
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default SectionWrapper
